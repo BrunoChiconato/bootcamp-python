b64eab684ccb383a587e629e0ed9eecfe45e4b01
Bruno Chiconato <brunochiconato01@gmail.com>
2024-08-19 21:39:59 -0300

    Update
    - Exercício 11 realizado

--- dia02/main.py
+++ dia02/main.py
@@ -57,8 +57,11 @@ area_circulo = 3.1415 * r**2
 print(f"A área do círculo de raio {r} é igual a: {area_circulo}")
 
 # #### Strings (`str`)
-
 # 11. Escreva um programa que receba uma string do usuário e a converta para maiúsculas.
+string = input("Escreva uma string: ")
+string_upper = string.upper()
+print(f"Sua string com todas as letras em maiúsculo é igual a: {string.upper}")
+
 # 12. Crie um programa que receba o nome completo do usuário e imprima o nome com todas as letras minúsculas.
 # 13. Desenvolva um programa que peça ao usuário para inserir uma frase e, em seguida, imprima esta frase sem espaços em branco no início e no final.
 # 14. Faça um programa que peça ao usuário para digitar uma data no formato "dd/mm/aaaa" e, em seguida, imprima o dia, o mês e o ano separadamente.
