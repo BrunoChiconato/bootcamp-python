7ccde7b334847b1358f23cc9a2a0a995ec6bad04
Bruno Chiconato <brunochiconato01@gmail.com>
2024-08-25 20:20:29 -0300

    Update:
    - Exercícios '3' e '4' realizados no arquivo 'main.py'

--- dia03/main.py
+++ dia03/main.py
@@ -46,67 +46,55 @@ else:
 # com severidade 'ERROR'. Dado um registro de log em formato de dicionário 
 # como `log = {'timestamp': '2021-06-23 10:00:00', 'level': 'ERROR', 'message': 'Falha na conexão'}`, 
 # escreva um programa que imprima a mensagem se a severidade for 'ERROR'.
+condicao = True
+lista_dict = []
+
+keys = ["timestamp", "level", "message"]
+
+while condicao:
+    novo_dict = {}
+
+    for key in keys:
+        value = input(f"Insira o valor para {key}: ")
+        novo_dict[key] = value
+
+    lista_dict.append(novo_dict)
+
+    resposta = input("Deseja inserir mais logs? (S/N): ")
+    if resposta.upper() == "N":
+        condicao = False
+    elif resposta.upper() != "S":
+        print("Insira apenas 'S' ou 'N'")
+
+for i, log in enumerate(lista_dict):
+    if log.get("level") == "ERROR":
+        print(f"Houve um erro no {i+1}º log: {log.get("message")}")
 
 ### Exercício 4: Validação de Dados de Entrada
 # Antes de processar os dados de usuários em um sistema de recomendação, 
 # você precisa garantir que cada usuário tenha idade entre 18 e 65 anos e tenha 
 # fornecido um email válido. Escreva um programa que valide essas condições 
 # e imprima "Dados de usuário válidos" ou o erro específico encontrado.
+condicao = True
 
-### Exercício 5: Detecção de Anomalias em Dados de Transações
-# Você está trabalhando em um sistema de detecção de fraude e precisa identificar 
-# transações suspeitas. Uma transação é considerada suspeita se o valor for superior 
-# a R$ 10.000 ou se ocorrer fora do horário comercial (antes das 9h ou depois das 18h). 
-# Dada uma transação como `transacao = {'valor': 12000, 'hora': 20}`, verifique se ela é suspeita.
+while condicao:
+    try:
+        idade = int(input("Insira sua idade: "))
+        email = input("Insira um e-mail válido: ").strip()
 
-### Exercício 6. Contagem de Palavras em Textos
-# Objetivo:** Dado um texto, contar quantas vezes cada palavra única aparece nele.
-
-### Exercício 7. Normalização de Dados
-# Objetivo:** Normalizar uma lista de números para que fiquem na escala de 0 a 1.
-
-### Exercício 8. Filtragem de Dados Faltantes
-# Objetivo:** Dada uma lista de dicionários representando dados de usuários, filtrar aqueles que têm um campo específico faltando
-
-### Exercício 9. Extração de Subconjuntos de Dados
-# Objetivo:** Dada uma lista de números, extrair apenas aqueles que são pares.
-
-### Exercício 10. Agregação de Dados por Categoria
-# Objetivo:** Dado um conjunto de registros de vendas, calcular o total de vendas por categoria.
-
-### Exercícios com WHILE
-
-### Exercício 11. Leitura de Dados até Flag
-# Ler dados de entrada até que uma palavra-chave específica ("sair") seja fornecida.
-
-### Exercício 12. Validação de Entrada
-# Solicitar ao usuário um número dentro de um intervalo específico até que a entrada seja válida.
-
-### Exercício 13. Consumo de API Simulado
-# Simular o consumo de uma API paginada, onde cada "página" de dados é processada em loop até que não haja mais páginas.
-
-### Exercício 14. Tentativas de Conexão
-# Simular tentativas de reconexão a um serviço com um limite máximo de tentativas.
-
-### Exercício 15. Processamento de Dados com Condição de Parada
-# Processar itens de uma lista até encontrar um valor específico que indica a parada.
-
-### Exercício 2: Classificação de Dados de Sensor
-# Imagine que você está trabalhando com dados de sensores IoT. 
-# Os dados incluem medições de temperatura. Você precisa classificar cada leitura 
-# como 'Baixa', 'Normal' ou 'Alta'. Considerando que:
-
-### Exercício 3: Filtragem de Logs por Severidade
-# Você está analisando logs de uma aplicação e precisa filtrar mensagens 
-# com severidade 'ERROR'. Dado um registro de log em formato de dicionário 
-# como `log = {'timestamp': '2021-06-23 10:00:00', 'level': 'ERROR', 'message': 'Falha na conexão'}`, 
-# escreva um programa que imprima a mensagem se a severidade for 'ERROR'.
-
-### Exercício 4: Validação de Dados de Entrada
-# Antes de processar os dados de usuários em um sistema de recomendação, 
-# você precisa garantir que cada usuário tenha idade entre 18 e 65 anos e tenha 
-# fornecido um email válido. Escreva um programa que valide essas condições 
-# e imprima "Dados de usuário válidos" ou o erro específico encontrado.
+        if idade < 18 or idade > 65:
+            print("Idade menor que 18 ou acima de 65.")
+        elif not email:
+            print("O campo 'e-mail' não pode estar vazio.")
+        elif any(char.isspace() for char in email):
+            print("Não insira espaços no campo 'e-mail'.")
+        elif "@" not in email or "." not in email:
+            print("O e-mail digitado no campo 'e-mail' precisa ser válido.")
+        else:
+            condicao = False
+            print("Dados de usuário válidos")
+    except ValueError:
+        print("Insira apenas números no campo 'idade'.")
 
 ### Exercício 5: Detecção de Anomalias em Dados de Transações
 # Você está trabalhando em um sistema de detecção de fraude e precisa identificar 
