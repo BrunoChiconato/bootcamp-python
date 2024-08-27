77a3f6cff078a6069145763c8bafa32e9d8a3f55
Bruno Chiconato <brunochiconato01@gmail.com>
2024-08-26 21:26:32 -0300

    Update:
    - Exercício 5 realizado no arquivo 'main.py'

--- dia03/main.py
+++ dia03/main.py
@@ -101,6 +101,39 @@ while condicao:
 # transações suspeitas. Uma transação é considerada suspeita se o valor for superior 
 # a R$ 10.000 ou se ocorrer fora do horário comercial (antes das 9h ou depois das 18h). 
 # Dada uma transação como `transacao = {'valor': 12000, 'hora': 20}`, verifique se ela é suspeita.
+condicao = True
+list_dict_transacao = []
+keys = ["valor", "hora"]
+
+while condicao:
+    dict_transacao = {}
+
+    for key in keys:
+        value = input(f"Informe o valor para o campo '{key}': ")
+
+        if value.isdigit():
+            dict_transacao[key] = value
+        else:
+            print("Insira apenas valores numéricos!")
+            break
+
+    if len(dict_transacao) == len(keys):
+        list_dict_transacao.append(dict_transacao)
+
+    resposta = input("Deseja inserir mais transações? (S/N): ")
+    if resposta.upper() == "N":
+        condicao = False
+    elif resposta.upper() != "S":
+        print("Insira apenas 'S' ou 'N'")
+
+for i, trans in enumerate(list_dict_transacao):
+    valor = float(trans.get("valor"))
+    hora = int(trans.get("hora"))
+
+    if valor > 10000:
+        print(f"Sua {i+1}ª transação é suspeita, pois o valor a ser transferido foi de: R$ {valor}")
+    elif hora < 9 or hora > 18:
+        print(f"Sua {i+1}ª transação é suspeita, pois a hora da transferência foi às: {hora}h")  
 
 ### Exercício 6. Contagem de Palavras em Textos
 # Objetivo:** Dado um texto, contar quantas vezes cada palavra única aparece nele.
