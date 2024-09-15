a9b77691ef164c361f996e53978f2544d821c7de
Bruno Chiconato <brunochiconato01@gmail.com>
2024-09-15 15:45:07 -0300

    Update
    - Exercícios 4 e 5 realizados no arquivo 'main.py'.

--- revisao01/src/main.py
+++ revisao01/src/main.py
@@ -15,4 +15,13 @@ print(f"Elementos do índice 2 ao 5: {lista_numeros[2:6]}")
 # 4. Adição e Remoção de Elementos 
 # Adicione o número 11 ao final da lista numeros e remova o número 5. 
 # Imprima a lista atualizada.
- 
+lista_numeros.append(11)
+print(f"Adicionando o número 11 a lista: {lista_numeros}")
+lista_numeros.remove(5)
+print(f"Removendo o numero 5 da lista: {lista_numeros}")
+
+# 6. List Comprehension 
+# Crie uma nova lista chamada quadrados que contenha o quadrado de cada número
+# em numeros usando list comprehension.
+quadrados = [num ** 2 for num in lista_numeros]
+print(f"Lista que contém o quadrado dos números em 'numeros': {quadrados}")
